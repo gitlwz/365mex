f972694ee22dd831a44a81c2edca53c1b9fbc617
liuwenzhu <547485261@qq.com>
2021-01-04 13:55:51 +0800

    主题

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,8 +1,9 @@
+/* eslint-disable no-unused-expressions */
 /*
  * @Author: 刘文柱 
  * @Date: 2018-10-18 10:09:05 
- * @Last Modified by:   刘文柱 
- * @Last Modified time: 2018-10-18 10:09:05 
+ * @Last Modified by: 刘文柱
+ * @Last Modified time: 2021-01-04 13:53:57
  */
 import { theme } from 'quant-ui';
 switch (theme.getCurrentColor()) {
